cd93cbc60c5ab743b5447c8c8d29531b97b13f0f
Daniel Tucker <Denyol@users.noreply.github.com>
2022-09-26 02:04:08 +0800

    Moved to use jQuery + responsive index layout

--- roulette.js
+++ roulette.js
@@ -5,191 +5,194 @@ const HARD = 20;
 const MED = 15;
 const EASY = 10;
 
-// Document constants
-const resultsTable = document.getElementById("results-div").children[0];
-const suButton = document.getElementById("straight");
-const splitButton = document.getElementById("split");
-const streetButton = document.getElementById("street");
-const cornerButton = document.getElementById("corner");
-const mixButton = document.getElementById("mix");
+$(function () {
+    // Document constants
+    const resultsTable = $("table");
+    const suButton = $("#straight");
+    const splitButton = document.getElementById("split");
+    const streetButton = document.getElementById("street");
+    const cornerButton = document.getElementById("corner");
+    const mixButton = document.getElementById("mix");
 
-const hardButton = document.getElementById("hard");
-const medButton = document.getElementById("med");
-const easyButton = document.getElementById("easy");
+    const hardButton = document.getElementById("hard");
+    const medButton = document.getElementById("med");
+    const easyButton = $("#easy");
 
-const form = document.getElementById("input-results").children[0];
+    const form = $("form");
 
-const gameRow = document.getElementsByClassName("button-box")[0];
-const modeRow = document.getElementsByClassName("button-box")[1];
+    const gameRow = $(".button-box").first();
+    const modeRow = $(".button-box").eq(1);
 
-let chips = [];
-let activeButtonHolder = {
-    type: null,
-    mode: null,
-}
+    let chips = [];
+    let activeButtonHolder = {
+        type: null,
+        mode: null,
+    }
 
-// Add a single chip
-function addChip(type, value) {
-    let chip = document.createElement("div");
-    chip.setAttribute("id", "chip");
-    chip.className = type;
-    chip.innerHTML = value;
-    document.getElementById("board-grid").appendChild(chip);
-    chips.push(chip);
-}
+    // Set up
+    setActive(suButton);
+    setActive(easyButton, true);
 
-function addChips(game) {
-    if (game instanceof GameMix) {
-        if (game.straightUps > 0)
-            addChip("straight", game.straightUps);
+    let game = new Game(EASY, "straight");
 
-        if (game.streets > 0)
-            addChip("street", game.streets);
+    addChips(game);
 
-        if (game.splits > 0) {
-            let split1 = Math.ceil(Math.random() * game.splits);
-            let split2 = game.splits - split1;
+    // Add a single chip
+    function addChip(type, value) {
+        let chip = document.createElement("div");
+        chip.setAttribute("id", "chip");
+        chip.className = type;
+        chip.innerHTML = value;
+        document.getElementById("board-grid").appendChild(chip);
+        chips.push(chip);
+    }
 
-            if (split1 > 0)
-                addChip("split", split1);
+    function addChips(game) {
+        if (game instanceof GameMix) {
+            if (game.straightUps > 0)
+                addChip("straight", game.straightUps);
 
-            if (split2 > 0) {
-                addChip("split2", split2);
-            }
-        }
+            if (game.streets > 0)
+                addChip("street", game.streets);
 
-        if (game.corners > 0) {
-            let corner1 = Math.ceil(Math.random() * game.corners);
-            let corner2 = game.corners - corner1;
+            if (game.splits > 0) {
+                let split1 = Math.ceil(Math.random() * game.splits);
+                let split2 = game.splits - split1;
 
-            if (corner1 > 0)
-                addChip("corner", corner1);
+                if (split1 > 0)
+                    addChip("split", split1);
 
-            if (corner2 > 0) {
-                addChip("corner2", corner2);
+                if (split2 > 0) {
+                    addChip("split2", split2);
+                }
             }
 
-        }
-    } else {
-        addChip(game.bet, game.getQuestion());
-    }
-}
+            if (game.corners > 0) {
+                let corner1 = Math.ceil(Math.random() * game.corners);
+                let corner2 = game.corners - corner1;
 
-function removeChips() {
-    for (let i = 0; i < chips.length; i++) {
-        let chip = chips[i];
-        chip.remove();
-    }
+                if (corner1 > 0)
+                    addChip("corner", corner1);
 
-    chips = [];
-}
+                if (corner2 > 0) {
+                    addChip("corner2", corner2);
+                }
 
-function setActive(button, mode = false) {
-    button.className = "active";
-    if (mode)
-        activeButtonHolder.mode = button;
-    else
-        activeButtonHolder.type = button;
-}
-
-function setInactive(mode = false) {
-    if (mode) {
-        let button = activeButtonHolder.mode;
-        button.classList.remove("active");
-        activeButtonHolder.mode = null;
-    }
-    else {
-        let button = activeButtonHolder.type;
-        button.classList.remove("active");
-        activeButtonHolder.type = null;
-    }
-
-}
-
-function newGame(diff, lastGame) {
-    if (lastGame instanceof GameMix)
-        return new GameMix(diff);
-    else
-        return new Game(diff, lastGame.bet)
-}
-
-// Set up
-
-setActive(suButton);
-setActive(easyButton, true);
-
-let game = new Game(EASY, "straight");
-
-addChips(game);
-
-function buttonListener(event, mode = false) {
-    let btn = event.target;
-
-    setInactive(mode);
-    setActive(btn, mode);
-
-    if (mode) {
-        switch (btn.id) {
-            case "easy":
-                game = newGame(EASY, game);
-                break;
-            case "med":
-                game = newGame(MED, game);
-                break;
-            case "hard":
-                game = newGame(HARD, game);
-                break;
-        }
-    } else if (btn.id == "mix") {
-        game = new GameMix(game.difficulty);
-    } else {
-        game = new Game(game.difficulty, btn.id);
-    }
-
-    removeChips();
-    addChips(game)
-}
-
-// Add button listeners to all buttons
-for (let i = 0; i < gameRow.childNodes.length; i++) {
-    let btn = gameRow.childNodes[i];
-    btn.addEventListener("click", function (e) {
-        buttonListener(e);
-    });
-}
-
-for (let i = 0; i < modeRow.childNodes.length; i++) {
-    let btn = modeRow.childNodes[i];
-    btn.addEventListener("click", function (e) {
-        buttonListener(e, true);
-    });
-}
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    let answer = form.elements["answer"].value;
-
-    if (answer.length > 0) {
-        let row = resultsTable.insertRow();
-
-        if (game instanceof GameMix)
-            row.insertCell().innerHTML = `${game.table}`;
-        else
-            row.insertCell().innerHTML = `${game.table} \u00D7 ${game.getQuestion()}`;
-        row.insertCell().innerHTML = answer;
-
-        if (game.verify(answer)) {
-            game.next();
-
-            row.insertCell().innerHTML = "Correct!";
-
-            removeChips();
-            addChips(game);
+            }
         } else {
-            row.insertCell().innerHTML = "Wrong!";
+            addChip(game.bet, game.getQuestion());
+        }
+    }
+
+    function removeChips() {
+        for (let i = 0; i < chips.length; i++) {
+            let chip = chips[i];
+            chip.remove();
         }
 
-        form.elements["answer"].value = "";
+        chips = [];
     }
+
+    function setActive(button, mode = false) {
+        button.addClass("active");
+
+        if (mode)
+            activeButtonHolder.mode = button;
+        else
+            activeButtonHolder.type = button;
+    }
+
+    function setInactive(mode = false) {
+        if (mode) {
+            let button = activeButtonHolder.mode;
+            button.removeClass("active");
+            activeButtonHolder.mode = null;
+        }
+        else {
+            let button = activeButtonHolder.type;
+            button.removeClass("active");
+            activeButtonHolder.type = null;
+        }
+
+    }
+
+    function newGame(diff, lastGame) {
+        if (lastGame instanceof GameMix)
+            return new GameMix(diff);
+        else
+            return new Game(diff, lastGame.bet)
+    }
+
+
+    function buttonListener(event, mode = false) {
+        let btn = $(event.target);
+        let btnId = btn.attr("id");
+
+        setInactive(mode);
+        setActive(btn, mode);
+
+        if (mode) {
+            switch (btnId) {
+                case "easy":
+                    game = newGame(EASY, game);
+                    break;
+                case "med":
+                    game = newGame(MED, game);
+                    break;
+                case "hard":
+                    game = newGame(HARD, game);
+                    break;
+            }
+        } else if (btnId == "mix") {
+            game = new GameMix(game.difficulty);
+        } else {
+            game = new Game(game.difficulty, btnId);
+        }
+
+        removeChips();
+        addChips(game)
+    }
+
+    // Add button listeners to all buttons
+    for (let i = 0; i < gameRow.children().length; i++) {
+        let btn = gameRow.children().eq(i);
+        btn.on("click", e => buttonListener(e));
+    }
+
+    for (let i = 0; i < modeRow.children().length; i++) {
+        let btn = modeRow.children().eq(i);
+        btn.on("click", e => buttonListener(e, true));
+    }
+
+    form.on("submit", function (e) {
+        e.preventDefault();
+
+        let answer = $("input#answer").val()
+
+        if (answer.length > 0) {
+            // [0] get DOM object
+            let s = resultsTable;
+            let row = resultsTable[0].insertRow();
+
+            if (game instanceof GameMix)
+                row.insertCell().innerHTML = `${game.table}`;
+            else
+                row.insertCell().innerHTML = `${game.table} \u00D7 ${game.getQuestion()}`;
+            row.insertCell().innerHTML = answer;
+
+            if (game.verify(answer)) {
+                game.next();
+
+                row.insertCell().innerHTML = "Correct!";
+
+                removeChips();
+                addChips(game);
+            } else {
+                row.insertCell().innerHTML = "Wrong!";
+            }
+
+            $("input#answer").val("");
+            $(".board-grid").focus();
+        }
+    });
 });
